bb29694f5649340888ca8fc8ad55a9e5e5932219
Robert <my4x4ca@hotmail.com>
2025-07-04 22:02:47 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# setup.py
+setup_text = """from setuptools import setup, find_packages
+
+setup(
+    name='btpie-n-the-middle',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'btpie = scripts.btpie:main'
+        ]
+    },
+)
