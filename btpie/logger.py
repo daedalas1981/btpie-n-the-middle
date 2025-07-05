b0ceee33c1f395747b249eec04b3aea4d988c392
Robert <my4x4ca@hotmail.com>
2025-07-04 23:06:47 -0700

    Update logger.py

--- btpie/logger.py
+++ btpie/logger.py
@@ -1,14 +1,21 @@
-logger_text = """import logging
+import logging
 import os
 
 def setup_logger(logfile):
     os.makedirs(os.path.dirname(logfile), exist_ok=True)
+    
     logger = logging.getLogger("btpie")
     logger.setLevel(logging.DEBUG)
 
-    handler = logging.FileHandler(logfile)
     formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
-    handler.setFormatter(formatter)
 
-    logger.addHandler(handler)
+    file_handler = logging.FileHandler(logfile)
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
     return logger
