d6bb6e8046c2db6241e952c62a1c1c02675de321
Robert <my4x4ca@hotmail.com>
2025-07-05 22:45:46 -0700

    Update __init__.py

--- btpie/__init__.py
+++ btpie/__init__.py
@@ -1,1 +1,9 @@
-init_text = """# Init for btpie package"""
+"""
+BTPIE-N-THE-MIDDLE
+
+Clean Python 3 Bluetooth Proxy-In-The-Middle (MITM) Tool for Raspberry Pi.
+
+Package initialization.
+"""
+
+__version__ = "0.1.0"
