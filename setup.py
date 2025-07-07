8000fc7fe167f99e0ba0eabccde3bcc242c093d2
Robert <my4x4ca@hotmail.com>
2025-07-07 15:10:26 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,33 @@
 # MIT License - Copyright (c) 2025 Robert Cole
 
-setup_text = """from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
 setup(
     name='btpie-n-the-middle',
-    version='0.1',
+    version='0.1.0',
+    author='Robert Cole',
+    description='Modern Bluetooth Proxy-In-The-Middle (MITM) Tool for Raspberry Pi',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/daedalas1981/btpie-n-the-middle',
     packages=find_packages(),
-    install_requires=[],
+    include_package_data=True,
+    install_requires=[
+        # Leave empty if system packages like pybluez are installed via apt
+        # 'pybluez>=0.23'  # Uncomment only if pip-managed pybluez is tested/needed
+    ],
     entry_points={
         'console_scripts': [
-            'btpie = scripts.btpie:main'
-        ]
+            'btpie=scripts.btpie:main',
+        ],
     },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Embedded Systems',
+    ],
+    python_requires='>=3.7',
 )
+
