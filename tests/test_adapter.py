6477080533ad6edf29e6402d3424c4491d0b71e5
Robert <my4x4ca@hotmail.com>
2025-07-22 03:33:17 -0700

    Update test_adapter.py

--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -1,32 +1,18 @@
 # MIT License - Copyright (c) 2025 Robert Cole
 
-import pytest
 from btpie.adapter import BluetoothAdapter
-
-def test_adapter_initialization():
-    adapter = BluetoothAdapter(master_mac="00:11:22:33:44:55", slave_mac="AA:BB:CC:DD:EE:FF", port=3)
-    
-    assert adapter.master_mac == "00:11:22:33:44:55"
-    assert adapter.slave_mac == "AA:BB:CC:DD:EE:FF"
-    assert adapter.port == 3
-    assert adapter.server_sock is None
-    assert adapter.client_sock is None
-    assert adapter.conn_sock is None
-
-def test_adapter_default_port():
-    adapter = BluetoothAdapter(master_mac="11:22:33:44:55:66", slave_mac="77:88:99:AA:BB:CC")
-    
-    assert adapter.port == 1
+from unittest.mock import MagicMock
 
 def test_adapter_reset_sockets():
     adapter = BluetoothAdapter(master_mac="11:22:33:44:55:66", slave_mac="77:88:99:AA:BB:CC")
-    
-    adapter.server_sock = "dummy"
-    adapter.client_sock = "dummy"
-    adapter.conn_sock = "dummy"
-    
+
+    # Use mocks instead of strings
+    adapter.server_sock = MagicMock()
+    adapter.client_sock = MagicMock()
+    adapter.conn_sock = MagicMock()
+
+    # Perform the close
     adapter.close()
-    
-    assert adapter.server_sock is None
-    assert adapter.client_sock is None
-    assert adapter.conn_sock is None
+
+    # Verify that .close() was called
+    adapter.conn_sock.close.assert_called_once()
